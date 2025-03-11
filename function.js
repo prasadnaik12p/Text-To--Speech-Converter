ca2fb53646f04ec0e353291458a876d153b67fbc
prasadnaik12p <mdbprasad223@gmail.com>
2025-03-11 15:51:35 +0530

    Fixed the select panel issue

--- function.js
+++ function.js
@@ -2,23 +2,33 @@ let speech = new SpeechSynthesisUtterance();
 let voices = [];
 let voiceSelect = document.querySelector("select");
 
+// Function to load voices and populate the select element
+function loadVoices() {
+    voices = window.speechSynthesis.getVoices();
+    if (voices.length > 0) {
+        speech.voice = voices[0]; // Default to the first voice
+        voiceSelect.innerHTML = ''; // Clear previous options
+        voices.forEach((voice, i) => {
+            let option = new Option(voice.name, i);
+            voiceSelect.add(option);
+        });
+    }
+}
 
-//Function of select element
-window.speechSynthesis.onvoiceschanged = () =>{
-    voices = window.speechSynthesis.getVoices(); //loades the list of voices from system
-    speech.voice = voices[0]; //speeks in defalut index value
-    voices.forEach((voice,i) => {
-        voiceSelect.options[i] = new Option(voice.name,i) //Addes multiple voices into select pannel
-    })
-};
+// Load voices when available
+if (window.speechSynthesis.onvoiceschanged !== undefined) {
+    window.speechSynthesis.onvoiceschanged = loadVoices;
+} else {
+    loadVoices();
+}
 
-voiceSelect.addEventListener("change",() =>{
-    speech.voice = voices[voiceSelect.value]
+// Change voice when a new option is selected
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
 });
 
-
-//Function of Listen button
-document.querySelector("button").addEventListener("click", ()=>{
-    speech.text = document.querySelector("textarea").value; //accesing the text from textarea
+// Function of Listen button
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 });
